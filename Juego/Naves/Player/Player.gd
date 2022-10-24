520e9404b967df10a6aeaaf12fad0d3ce084be85
LucasEscribe <lfd.meneses@gmail.com>
2022-10-24 10:28:47 -0300

    desastre total
    Nave inmovilizada

--- Juego/Naves/Player/Player.gd
+++ Juego/Naves/Player/Player.gd
@@ -1,8 +1,6 @@
 class_name Player
 extends RigidBody2D
 
-Eventos.emit_signal("nave_destruida", global_position, 3)
-
 #Enums
 enum ESTADO {SPAWN, VIVO, INVENCIBLE, MUERTO}
 
@@ -73,7 +71,7 @@ func controlador_estados(nuevo_estado: int) -> void:
 		ESTADO.MUERTO:
 			colisionador.set_deferred("disabled", true)
 			canion.set_puede_disparar(true)
-			Eventos.emit_signal("nave_destruida", global_position)
+			Eventos.emit_signal("nave_destruida", global_position, 3)
 			queue_free()
 		_:
 			printerr("Error de estado")
--- Juego/Proyectiles/Proyectil.gd
+++ Juego/Proyectiles/Proyectil.gd
@@ -1,4 +1,3 @@
-#Proyectil.gd
 class_name Proyectil
 extends Area2D
 
@@ -11,10 +10,23 @@ func crear(pos: Vector2, dir: float, vel: float, danio_p: int) -> void:
 	position = pos
 	rotation = dir
 	velocidad = Vector2(vel, 0).rotated(dir)
+	danio = danio_p
 	
 ## Métodos
 func _physics_process(delta: float) -> void:
 	position += velocidad * delta
 
+# Métodos Custom
+func daniar(otro_cuerpo: CollisionObject2D) -> void:
+	if otro_cuerpo.has_method("recibir_danio"):
+		otro_cuerpo.recibir_danio(danio)
+
+# Señales Internas
 func _on_VisibilityNotifier2D_screen_exited() -> void:
+	queue_free()
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.has_method("recibir_danio"):
+		area.recibir_danio(danio)
+	
 	queue_free()
