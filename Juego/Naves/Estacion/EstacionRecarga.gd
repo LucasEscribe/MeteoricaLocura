d2409720bcd79e9cd4abfe452a3c5359579057c5
LucasEscribe <lfd.meneses@gmail.com>
2022-11-02 22:54:02 -0300

    Estoy a medias con la ER
    ER = Estación de Recarga

---
+++ Juego/Naves/Estacion/EstacionRecarga.gd
@@ -0,0 +1,47 @@
+
+class_name EstacionRecarga
+extends Node2D
+
+##Atributos Export
+export var energia:float = 6.0
+export var radio_energia_entregada: float = 0.05
+
+## Atributos
+var nave_player: Player = null
+var player_en_zona: bool = false
+
+## Metodos
+func _unhandled_input(event: InputEvent) -> void:
+	if not puede_recargar(event):
+		return
+
+	energia -= radio_energia_entregada
+
+	if event.is_action("recargar_escudo"):
+		nave_player.get_escudo().controlar_energia(radio_energia_entregada)
+	elif event.is_action("recarga_laser"):
+		nave_player.get_laser().controlar_energia(radio_energia_entregada)
+
+## Métodos Custom
+func puede_recargar(event: InputEvent) -> bool:
+	var hay_input = event.is_action("recargar_escudo") or event.is_action("recargar_laser")
+	if hay_input and player_en_zona and energia > 0.0:
+		return true
+	
+	return false
+
+
+## Señales Internas
+func _on_AreaColision_body_entered(body: Node) -> void:
+	if body.has_method("destruir"):
+		body.destruir()
+
+func _on_AreaRecarga_body_entered(body: Node) -> void:
+	if body is Player:
+		player_en_zona = true
+		nave_player = body
+
+	body.set_gravity_scale(0.1)
+
+func _on_AreaRecarga_body_exited(body: Node) -> void:
+	body.set_gravity_scale(0.0)
